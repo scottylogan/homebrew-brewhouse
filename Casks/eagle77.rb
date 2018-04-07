d988dfde029c0e289c119f46c79f3d04c69040dc
Scotty Logan <scotty@catbert.net>
2018-04-06 18:57:38 -0700

    added old Eagle

---
+++ Casks/eagle77.rb
@@ -0,0 +1,16 @@
+cask '/Users/scotty/src/homebrew-brewhouse/Casks/eagle' do
+  version '7.7.0'
+  sha256 '4f6aae6e2b56f3da4bd24940ac4f09e838a2b3105c5ff506b80825a8a1250022'
+
+  url 'http://eagle.autodesk.com/eagle/download-software/8'
+  name 'Eagle v7.7.0'
+  homepage 'http://eagle.autodesk.com/eagle/software-versions/1'
+
+  pkg "eagle-mac64-${version}.pkg"
+
+  uninstall pkgutil: [
+                       "com.CadSoftComputerGmbH.EAGLE-#{version}-64"
+                     ],
+            delete:  "#{appdir}/EAGLE-#{version}"
+
+end
