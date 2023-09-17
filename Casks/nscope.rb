21e46c2b380ceda512e7431e8c4538e776ed1145
Scotty Logan <scotty@catbert.net>
2023-09-17 14:36:50 -0700

    removed appcast from nscope cask

--- Casks/nscope.rb
+++ Casks/nscope.rb
@@ -3,7 +3,7 @@ cask 'nscope' do
   sha256 '79de8932422d4750539752dd7c263123fa46bbb15b693408b7a8f74939bec817'
 
   url "https://www.nscope.org/v#{version}/mac/nScope.dmg"
-  appcast 'https://www.nscope.org/download'
+#  appcast 'https://www.nscope.org/download'
   name 'nScope'
   homepage 'https://www.nscope.org/'
 
