a8958dbc1b2bd934d5b7dba15fe8db80346a8893
Scotty Logan <swl@stanford.edu>
2024-08-23 13:24:45 -0700

    updated ec2-tools

--- Formula/ec2-tools.rb
+++ Formula/ec2-tools.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Ec2Tools < Formula
   desc "EC2 Tools"
   homepage "https://github.com/scottylogan/ec2-tools/"
-  url "https://github.com/scottylogan/ec2-tools/archive/v0.2.2.tar.gz"
-  sha256 "90961356ff690d6c66b08c7423428fb45883f290fa2841511f5373adbce33194"
+  url "https://github.com/scottylogan/ec2-tools/archive/v0.2.3.tar.gz"
+  sha256 "43a38cb8b7bb792069d1b4c61a8ce17038869fd06d282e156afbbaf6de9fc46f"
 
   depends_on "node"
 
