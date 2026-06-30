37596913fd6b70c3555ebc8ba6057fcbaa9dab34
Scotty Logan <swl@slac.stanford.edu>
2026-06-30 16:22:32 -0700

    fixed ldap-scripts version

--- Formula/ldap-scripts.rb
+++ Formula/ldap-scripts.rb
@@ -1,7 +1,7 @@
 class LdapScripts < Formula
   desc "LDAP Scripts"
   homepage "https://github.com/scottylogan/ldap-scripts/"
-  url "https://github.com/scottylogan/ldap-scripts/archive/v0.1.0.tar.gz"
+  url "https://github.com/scottylogan/ldap-scripts/archive/v0.1.1.tar.gz"
   sha256 "c1cda7401130f289d33ffe03e79cd29f79b74cc0941d3db786331509e084698a"
 
   def install
